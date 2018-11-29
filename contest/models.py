5ed391d0698395ae1ec05e32e335656ddd92a6d5
ruth1998bigman@gmail.com <ruth1998bigman@gmail.com>
2018-11-29 20:09:37 +0800

    加一个contest-problem

--- contest/models.py
+++ contest/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from problems.models import Problem
 # Create your models here.
 # from problems.models import Problem
 
@@ -8,7 +9,7 @@ class Contest(models.Model):
     '''
     基本比赛对象
     '''
-    id = models.CharField(max_length=20, unique=True, primary_key=True, verbose_name='比赛编号')
+    contest_id = models.CharField(max_length=20, unique=True, primary_key=True, verbose_name='比赛编号')
 
     title = models.CharField(max_length=1000, verbose_name='比赛标题', blank=True)
 
@@ -24,3 +25,16 @@ class Contest(models.Model):
 
     def __str__(self):
         return self.id
+
+
+
+
+class  Contest_problem(models.Model):
+    '''
+    比赛与题目的连接
+    '''
+    # problem_id = models.CharField(max_length=20,primary_key=True)
+    problem_id = models.ForeignKey(Problem)
+    contest_id = models.ForeignKey(Contest,primary_key=True)
+
+
